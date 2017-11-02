baa7641a37ff37f837e5f71bc8623fef03ee0a04
Kalyan Parajuli <kalya.parajuli@trincoll.edu>
2017-11-02 01:50:39 -0400

    Project Setup with Firebase Database

---
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/CreateListingsActivity.java
@@ -0,0 +1,13 @@
+package com.krparajuli.collegethrift;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateListingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_listings);
+    }
+}

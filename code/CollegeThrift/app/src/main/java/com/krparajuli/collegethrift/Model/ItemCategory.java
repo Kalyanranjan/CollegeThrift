6e8583490fda7a07e4711ea6e3dc36028f2cd88f
Kalyan R. Parajuli <kalyanranjanp@gmail.com>
2018-02-02 13:00:30 -0500

    Added Category on Backend

---
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/Model/ItemCategory.java
@@ -0,0 +1,15 @@
+package com.krparajuli.collegethrift.Model;
+
+/**
+ * Created by kal on 2/2/18.
+ */
+
+public enum ItemCategory {
+    BOOKS,
+    STATIONERY,
+    GADGET,
+    APPLIANCE,
+    CLOTHING,
+    KITCHEN,
+    OTHER
+}

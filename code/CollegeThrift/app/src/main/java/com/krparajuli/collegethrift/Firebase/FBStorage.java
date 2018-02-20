4cc883b7e35e0ecdcbabaec0b16aac30b0b71074
Kalyan Ranjan Parajuli <kalyan.parajuli@trincoll.edu>
2018-02-20 14:47:21 -0500

    Added FBStorage Helper Class

---
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/Firebase/FBStorage.java
@@ -0,0 +1,74 @@
+package com.krparajuli.collegethrift.Firebase;
+
+import android.util.Log;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+/**
+ * Created by kal on 2/20/18.
+ */
+
+public class FBStorage {
+    private static String LISTING_THUMBNAILS_ROOT = "listing-thumbnails/";
+    private static String LISTING_IMAGES_ROOT = "listing-images/";
+    private static String USER_PROFILES_ROOT = "user-profiles/";
+
+    private static StorageReference mStorageRef = null;
+    private static StorageReference mListingThumbnailRef = null;
+    private static StorageReference mListingImagesRef = null;
+    private static StorageReference mUserProfileRef = null;
+
+    private static String mStorageError = "Storage Error";
+    private static String mStorageSuccess = "Storage Success";
+
+    private static StorageReference instantiateStorage() {
+        if (mStorageRef == null)
+            mStorageRef = FirebaseStorage.getInstance().getReference();
+        if (mStorageRef == null)
+            storageErrorDisplay();
+
+        return mStorageRef;
+    }
+
+    private static StorageReference getListingThumbnailReference() {
+        instantiateStorage();
+        if (mStorageRef == null)
+                return null;
+        if (mListingThumbnailRef == null)
+            mListingThumbnailRef = mStorageRef.child(LISTING_THUMBNAILS_ROOT);
+        if (mListingThumbnailRef == null)
+            return null;
+
+        return mListingThumbnailRef;
+    }
+
+    private static StorageReference getListingImagesReference() {
+        instantiateStorage();
+        if (mStorageRef == null)
+            return null;
+        if (mListingImagesRef == null)
+            mListingImagesRef = mStorageRef.child(LISTING_THUMBNAILS_ROOT);
+        if (mListingImagesRef == null)
+            return null;
+
+        return mListingImagesRef;
+    }
+
+    private static StorageReference getUserProfileReference() {
+        instantiateStorage();
+        if (mStorageRef == null)
+            return null;
+        if (mUserProfileRef == null)
+            mUserProfileRef = mStorageRef.child(LISTING_THUMBNAILS_ROOT);
+        if (mUserProfileRef == null)
+            return null;
+
+        return mUserProfileRef;
+    }
+
+
+    private static void storageErrorDisplay() {
+        Log.v("FBStorage", "Storage Connection Error");
+    }
+}

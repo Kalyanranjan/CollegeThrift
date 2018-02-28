9f0d4f1155cb85f990d0682917c88c36f64cf44a
Kalyan Ranjan Parajuli <kalyan.parajuli@trincoll.edu>
2018-02-27 23:53:53 -0500

    Get Create Listings Back

--- code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activity/DrawerViewListingsActivity.java
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activity/DrawerViewListingsActivity.java
@@ -67,8 +67,8 @@ public class DrawerViewListingsActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               Intent createListingsIntent = new Intent(DrawerViewListingsActivity.this, CreateListingsActivity.class);
+               startActivity(createListingsIntent);
             }
         });
 
@@ -127,19 +127,19 @@ public class DrawerViewListingsActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);

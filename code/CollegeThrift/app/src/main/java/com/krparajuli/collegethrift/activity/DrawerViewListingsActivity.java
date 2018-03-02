90e296d6231b8ab6b0353c12da36fadecee138ed
Kalyan Ranjan Parajuli <kalyan.parajuli@trincoll.edu>
2018-03-02 13:40:31 -0500

    Bug Fix

--- code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activity/DrawerViewListingsActivity.java
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activity/DrawerViewListingsActivity.java
@@ -137,6 +137,7 @@ public class DrawerViewListingsActivity extends AppCompatActivity
             case R.id.dlvd_nav_my_listings:
                 Intent myListingsIntent = new Intent(DrawerViewListingsActivity.this, ScrollerMyListingsActivity.class);
                 startActivity(myListingsIntent);
+                break;
             default:
                 DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
                 drawer.closeDrawer(GravityCompat.START);

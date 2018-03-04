a94ee3c1d21ae1f27b54a4c74310a0b8e44d4fb0
Kalyan Ranjan Parajuli <kalyan.parajuli@trincoll.edu>
2018-03-04 16:54:32 -0500

    Added DashboardActivity

---
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activity/DashboardActivity.java
@@ -0,0 +1,17 @@
+package com.krparajuli.collegethrift.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.krparajuli.collegethrift.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+    }
+}
--- code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activity/DrawerViewListingsActivity.java
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activity/DrawerViewListingsActivity.java
@@ -134,11 +134,15 @@ public class DrawerViewListingsActivity extends AppCompatActivity
                 startActivity(loginIntent);
                 finish();
                 break;
-            case R.id.dlvd_nav_my_listings:
-            default:
-                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-                drawer.closeDrawer(GravityCompat.START);
+            case R.id.dlvd_nav_dashboard:
+                Intent dashboardIntent = new Intent(DrawerViewListingsActivity.this, DashboardActivity.class);
+                startActivity(dashboardIntent);
+                closeDrawer();
                 break;
+            default:
+                closeDrawer();
+                break;
+
         }
 //        if (id == R.id.nav_camera) {
 //            // Handle the camera action
@@ -158,4 +162,8 @@ public class DrawerViewListingsActivity extends AppCompatActivity
         return true;
     }
 
+    private void closeDrawer() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+    }
 }

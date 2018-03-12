cea23317caa93bd4d5670c607ff0f2e050f85359
Kalyan Ranjan Parajuli <kalyan.parajuli@trincoll.edu>
2018-03-12 14:43:30 -0400

    Added indexListingsToElastic firebase function to Google Cloud ES
    This file is for deployment to Google Cloud Only

--- code/CollegeThrift/functions/index.js
+++ code/CollegeThrift/functions/index.js
@@ -1,8 +1,37 @@
 const functions = require('firebase-functions');
 
+const request = require('request-promise');
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.indexListingsToElastic = functions.database.ref("/listings/{listing_id}").onWrite(event => {
+	let listingData = event.data.val();
+	let listing_id = event.params.listing_id;
+
+	console.log('Indexing Listing', listingData);
+
+	let elasticSearchConfig = functions.config().elasticsearch;
+	let elasticSearchUrl = elasticSearchConfig.url + 'listings/listing/' + listing_id;
+	let elasticSearchMethod = listingData ? 'POST':'DELETE';
+
+
+	let elasticSearchRequest = {
+		url: elasticSearchUrl,
+		auth: {
+			username: elasticSearchConfig.username,
+			password: elasticSearchConfig.password
+		},
+		body: listingData,
+		json: true
+	};
+
+	return request(elasticSearchRequest).then(response => {
+		console.log("ElasticSearch response", response);
+		return 0;
+	});
+});  

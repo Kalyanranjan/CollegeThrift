a1748ec67c525c616c8f6ed77354035b7077e6d4
Kalyan Ranjan Parajuli <kalyan.parajuli@trincoll.edu>
2018-03-20 23:11:40 -0400

    Make Basic Elastic Search Query Functional

--- code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activity/SearchActivity.java
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activity/SearchActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,22 +23,39 @@ import com.google.firebase.database.ValueEventListener;
 import com.krparajuli.collegethrift.R;
 import com.krparajuli.collegethrift.model.Listing;
 import com.krparajuli.collegethrift.model.ListingCategory;
+import com.krparajuli.collegethrift.model.ListingHitsList;
+import com.krparajuli.collegethrift.model.ListingHitsObject;
 import com.krparajuli.collegethrift.model.ListingType;
+import com.krparajuli.collegethrift.utils.ElasticSearchAPI;
 import com.krparajuli.collegethrift.viewholder.ListingViewHolder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import okhttp3.Credentials;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SearchActivity extends AppCompatActivity {
 
     private static final String TAG = "SearchActivity";
+    private static final String BASE_URL = "http://35.225.53.194//elasticsearch/";
 
     // [START define_database_reference]
     private DatabaseReference mDatabase;
     // [END define_database_reference]
 
     private FirebaseAuth mAuth;
-
     private Query mQuery = null;
 
     private String mElasticSearchPassword;
+
+    private ArrayList<Listing> mListings;
 
     private FirebaseRecyclerAdapter<Listing, ListingViewHolder> mAdapter;
     private RecyclerView mRecycler;
@@ -61,6 +79,8 @@ public class SearchActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         // [END create_database_reference]
 
+        getElasticSearchPasword();
+
         mRecycler = (RecyclerView) findViewById(R.id.ls_recycler_view);
         mManager = new LinearLayoutManager(this);
         mManager.setReverseLayout(true);
@@ -83,6 +103,7 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 prepareQuery(mDatabase);
+
                 mAdapter = new FirebaseRecyclerAdapter<Listing, ListingViewHolder>(Listing.class, R.layout.listing_item,
                         ListingViewHolder.class, mQuery) {
 
@@ -145,7 +166,7 @@ public class SearchActivity extends AppCompatActivity {
 
         Query query = FirebaseDatabase.getInstance().getReference()
                 .child("elasticSearch")
-                .child("password");
+                .orderByValue();
 
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -156,7 +177,7 @@ public class SearchActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.d(TAG, "DatabaseError: " + databaseError.getMessage());
             }
         });
     }
@@ -164,24 +185,93 @@ public class SearchActivity extends AppCompatActivity {
 
 
 
-
     private void prepareQuery(DatabaseReference databaseReference) {
-        Query prepQuery = databaseReference.child("listings");
+        mListings = new ArrayList<Listing>();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ElasticSearchAPI searchAPI = retrofit.create(ElasticSearchAPI.class);
+
+        HashMap<String, String> headerMap = new HashMap<>();
+        headerMap.put("Authorization", Credentials.basic("user", mElasticSearchPassword));
+        Log.v(TAG, "Credential = "+mElasticSearchPassword);
+
+        String searchKeywordString = "";
+        if (!lsKeywordText.equals("")) {
+            searchKeywordString += lsKeywordText.getText().toString() + "*";
+        }
 //        if (mCategorySearch) {
-//            prepQuery = prepQuery.orderByChild("category").equalTo(ListingCategory.values()[lsCategorySpinner.getSelectedItemPosition()].toString());
+//            searchKeywordString += " category:" + ListingCategory.values()[lsCategorySpinner.getSelectedItemPosition()].toString();
 //        }
+//        if (mTypeSearch) {
+//            searchKeywordString += " type:" + ListingType.values()[lsTypeSpinner.getSelectedItemPosition()].toString();
+//        }
+
+        Call<ListingHitsObject> call = searchAPI.search(headerMap, "AND", searchKeywordString);
+        call.enqueue(new Callback<ListingHitsObject>() {
+            @Override
+            public void onResponse(Call<ListingHitsObject> call, Response<ListingHitsObject> response) {
+                ListingHitsList listingHitsList = new ListingHitsList();
+                String jsonResponse = "";
+
+                try {
+                    Log.d(TAG,"onResponse: server response: "+response.toString());
+                    if (response.isSuccessful()) {
+                        listingHitsList = response.body().getHits();
+                    } else {
+                        jsonResponse = response.errorBody().string();
+                    }
+
+                    Log.d(TAG, "onResponse: hits: "+ listingHitsList);
+
+                    for (int i=0; i<listingHitsList.getListingIndex().size(); i++) {
+                        mListings.add(listingHitsList.getListingIndex().get(i).getListing());
+                    }
+
+                    Log.d(TAG, "onResponse: size: " + mListings.size());
+
+                } catch (NullPointerException e) {
+                    Log.v(TAG, "onResponse: NullPointerException: " + e.getMessage());
+                } catch (IndexOutOfBoundsException e) {
+                    Log.v(TAG, "onResponse: IndexOutOfBoundsException:  " + e.getMessage());
+                } catch (IOException e) {
+                    Log.v(TAG, "onResponse: IOException:  " + e.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ListingHitsObject> call, Throwable t) {
+                Log.e(TAG, "onFailure: "+t.getMessage());
+//                Toast.makeText(getCallingActivity(), "search failed", Toast.LENGTH_SHORT).show(); //getActivity() gives error
+            }
+        });
+
+
+
+
+
+        Query prepQuery = databaseReference.child("listings");
+        if (mCategorySearch) {
+            prepQuery = prepQuery.orderByChild("category").equalTo(ListingCategory.values()[lsCategorySpinner.getSelectedItemPosition()].toString());
+        }
 //        if (mTypeSearch) {
 //            prepQuery = prepQuery.orderByChild("type").equalTo(ListingType.values()[lsTypeSpinner.getSelectedItemPosition()].toString());
 //        }
         // The integer might not be able to parsed correctly here
-        if (mPriceSearch) {
-            if (!lsPriceFrom.getText().toString().equals(""))
-                prepQuery = prepQuery.orderByChild("price").startAt(lsPriceFrom.getText().toString());
+//        if (mPriceSearch) {
+//            if (!lsPriceFrom.getText().toString().equals(""))
+//                prepQuery = prepQuery.orderByChild("price").startAt(lsPriceFrom.getText().toString());
 
             //if (!lsPriceTo.getText().toString().equals(""))
             //    prepQuery = prepQuery.orderByChild("price").endAt(lsPriceTo.getText().toString());
-        }
+//        }
         mQuery = prepQuery;
+
+
+
     }
 
     private void disableFilterOptions() {

5a7bfd6c0d03d507a5060a25742d0ed26cff923e
Kalyan Ranjan Parajuli <kalyan.parajuli@trincoll.edu>
2018-03-22 23:08:27 -0400

    Adding Background Image Resizer Scaffold

---
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/utils/ImageUploader.java
@@ -0,0 +1,15 @@
+package com.krparajuli.collegethrift.utils;
+
+import android.net.Uri;
+
+/**
+ * Created by kal on 3/22/18.
+ */
+
+public class ImageUploader {
+
+    public static void uploadImage(Uri imageUri) {
+
+    }
+
+}

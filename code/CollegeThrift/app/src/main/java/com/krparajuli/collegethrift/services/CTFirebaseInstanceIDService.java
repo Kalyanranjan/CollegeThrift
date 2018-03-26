22b416f66720ebeca51335146594489d5badea35
Kalyan Ranjan Parajuli <kalyan.parajuli@trincoll.edu>
2018-03-25 22:24:16 -0400

    Added FirebaseInstanceID and FirebaseMessagingService classes

---
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/services/CTFirebaseInstanceIDService.java
@@ -0,0 +1,14 @@
+package com.krparajuli.collegethrift.services;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by kal on 3/25/18.
+ */
+
+public class CTFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+    }
+}

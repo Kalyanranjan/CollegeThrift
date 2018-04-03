62068158af57ab82b8a6e4340caddb0ba3c35f29
Kalyan Ranjan Parajuli <kalyan.parajuli@trincoll.edu>
2018-04-03 18:17:50 -0400

    Added Message Front End

--- code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activities/MessengerActivity.java
+++ code/CollegeThrift/app/src/main/java/com/krparajuli/collegethrift/activities/MessengerActivity.java
@@ -4,16 +4,20 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Handler;
-import android.os.Message;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.github.bassaer.chatmessageview.model.ChatUser;
+import com.github.bassaer.chatmessageview.model.Message;
 import com.github.bassaer.chatmessageview.util.ChatBot;
+import com.github.bassaer.chatmessageview.model.IChatUser;
 import com.github.bassaer.chatmessageview.view.ChatView;
 import com.krparajuli.collegethrift.R;
-import com.krparajuli.collegethrift.models.User;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 
@@ -38,8 +42,8 @@ public class MessengerActivity extends AppCompatActivity {
         Bitmap yourIcon = BitmapFactory.decodeResource(getResources(), R.drawable.face_1);
         String yourName = "Emily";
 
-//        final User me = new User(myId, myName, myIcon);
-//        final User you = new User(yourId, yourName, yourIcon);
+        final ChatUser me = new ChatUser(myId, myName, myIcon);
+        final ChatUser you = new ChatUser(yourId, yourName, yourIcon);
 
         mChatView = (ChatView)findViewById(R.id.chat_view);
 
@@ -47,7 +51,7 @@ public class MessengerActivity extends AppCompatActivity {
         mChatView.setRightBubbleColor(ContextCompat.getColor(this, R.color.green500));
         mChatView.setLeftBubbleColor(Color.WHITE);
         mChatView.setBackgroundColor(ContextCompat.getColor(this, R.color.blueGray500));
-        //mChatView.setSendButtonColor(ContextCompat.getColor(this, R.color.cyan500));
+        mChatView.setSendButtonColor(ContextCompat.getColor(this, R.color.green500));
         mChatView.setSendIcon(R.drawable.ic_action_send);
         mChatView.setRightMessageTextColor(Color.WHITE);
         mChatView.setLeftMessageTextColor(Color.BLACK);
@@ -62,34 +66,35 @@ public class MessengerActivity extends AppCompatActivity {
         mChatView.setOnClickSendButtonListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 //new message
-//                Message message = new Message.Builder()
-//                        .setUser(me)
-//                        .setRightMessage(true)
-//                        .setMessageText(mChatView.getInputText())
-//                        .hideIcon(true)
-//                        .build();
-//                //Set to chat view
-//                mChatView.send(message);
-                //Reset edit text
+                Message message = new Message.Builder()
+                        .setUser(me)
+                        .setRight(true)
+                        .setText(mChatView.getInputText())
+                        .hideIcon(true)
+                        .build();
+                //Set to chat view
+                mChatView.send(message);
+//                //Reset edit text
                 mChatView.setInputText("");
 
                 //Receive message
-//                final Message receivedMessage = new Message.Builder()
-//                        .setUser(you)
-//                        .setRightMessage(false)
-//                        .setMessageText(ChatBot.talk(me.getName(), message.getMessageText()))
-//                        .build();
-//
+                final Message receivedMessage = new Message.Builder()
+                        .setUser(you)
+                        .setRight(false)
+                        .setText(ChatBot.INSTANCE.talk(me.getName(), message.getText()))
+                        .build();
+
 //                // This is a demo bot
 //                // Return within 3 seconds
-//                int sendDelay = (new Random().nextInt(4) + 1) * 1000;
-//                new Handler().postDelayed(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        mChatView.receive(receivedMessage);
-//                    }
-//                }, sendDelay);
+                int sendDelay = (new Random().nextInt(4) + 1) * 1000;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mChatView.receive(receivedMessage);
+                    }
+                }, sendDelay);
             }
 
         });
